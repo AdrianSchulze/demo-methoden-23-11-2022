a442bdea4b60309c0013b426a1b096f7686c28c0
AdrianSchulze <adrian@sei-sicherer.de>
2022-11-23 11:44:57 +0100

    Scan method without other methods

--- Main.java
+++ Main.java
@@ -1,27 +1,12 @@
 import java.util.Scanner;
 
 public class Main {
-
-    public static int sum (int number1, int number2) {
-        return number1 + number2;
-    }
-
-    public static void sum2 (int number1, int number2) {
-        System.out.println("Summe: " + sum(number1, number2));
-    }
-
     public static void printInput (int num1, int num2) {
-        System.out.println(num1 + num2);
+        int sum = num1 + num2;
+        System.out.println("Die Summe der eingegebenen Zahlen beträgt: " + sum);
     }
 
     public static void main(String[] args){
-        int num1 = 10;
-        int num2 = 20;
-        System.out.println(sum(num1, num2));
-
-        int num3 = 30;
-        int num4 = 40;
-        sum2(num3, num4);
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Geben Sie eine Nummer ein und drücken Sie ENTER:");
